6fec57e20eb432cae4f31dd6a35b953fbb99ec18
DarccMan <darcy3057@gmail.com>
2022-06-25 15:16:08 +1000

    Add cors

--- index.ts
+++ index.ts
@@ -3,6 +3,17 @@ import express from "express";
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// Allow CORS
+//! Change to strict domain
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept",
+  );
+  next();
+});
+
 app.get("/", (req, res) => {
   res.status(200).send("Hello!");
 });
